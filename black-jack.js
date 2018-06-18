113959442f46cf61b55504074af3495d3ead327e
Julia Larionova <jul.larionova@gmail.com>
2018-06-19 00:30:32 +0800

    Исправлены синтаксические ошибки, поправлена getSum

--- black-jack.js
+++ black-jack.js
@@ -4,18 +4,13 @@ function getSum(hand) {
     var sum = 0;
     for (var i = 0; i < hand.length; i++) {
         var card = hand [i];
-        if (card = 'A') {
+        if (card != 'A') {
             if (card == 'J' || card == 'Q' || card == 'K') {
                 sum = sum + 10;
-
             } else {
                 sum = sum + parseInt(card);
             }
-        }
-    }
-    for (var i = 0; i < hand.length; i++) {
-        var card = hand [i];
-        if (card == 'A') {
+        } else {
             if (sum > 10) {
                 sum = +1;
             } else {
@@ -26,50 +21,51 @@ function getSum(hand) {
     return sum;
 }
 
+function getCard() {
+    // TODO: Написать функцию которая возвращает карту
+    return 7
+}
+
+var dealer = [];
+var player = [];
+
+function getStatus() {
+    return 'Дилер: ' + dealer.join() + ' Игрок: ' + player.join() + '.';
+}
+
 alert('Дьявольское везение! Black Jack на раздаче! $_$');
 var answer = '';
 do {
-    answer = prompt(getStatus() + 'Хотите еще карту? 1 - да, иначе - нет');
+    answer = prompt(getStatus() + ' Хотите еще карту? 1 - да, иначе - нет');
+
     if (answer == '1') {
         player.push(getCard());
     }
-var dealer = [getCard();
-var dealer = [getCard(), getCard()];
 
-function getStatus() {
-    return 'Дилер:' + dealer.join('') + 'Игрок:' + player.join('') + '.';
-}
-
-var dealer = [getCard()];
-var player = [getCard(), getCard()];
-if (getSum(player) == 21) {
-}
-sum = getSum(player);
-if (sum > 21) {
-    alert('Перебор' + getStatus());
-    break;
-} else if (sum == 21) {
-    alert('Black Jack!' + getStatus());
-    break;
-}
-}else
-{
-    while (getSum(dealear) < 17) {
-        dealer.push(getCard());
+    sum = getSum(player);
+    if (sum > 21) {
+        alert('Перебор ' + getStatus());
+        break;
+    } else if (sum == 21) {
+        alert('Black Jack! ' + getStatus());
+        break;
     }
+} while (answer == '1');
+
+while (getSum(dealer) < 17) {
+    dealer.push(getCard());
 }
-var SumDealear = getSum(dealer);
-var SumPlayer = getSum(player);
-if (SumDealer == 21) {
-    alert('У дилера перебор' + getStatus());
+
+var sumDealer = getSum(dealer);
+var sumPlayer = getSum(player);
+if (sumDealer > 21) {
+    alert('У дилера перебор ' + getStatus());
 } else if (sumPlayer == sumDealer) {
-    alert('Ничья!' + getStatus());
-} else if (SumPlayer > sumDealer) {
-    alert('Выигрыш') + getStatus();
+    alert('Ничья! ' + getStatus());
+} else if (sumPlayer > sumDealer) {
+    alert('Выигрыш ' + getStatus());
 } else {
-    alert('Проигрыш') + getStatus();
+    alert('Проигрыш ' + getStatus());
 }
-}
-while (answer == '1');
 
 
