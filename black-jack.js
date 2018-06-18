3764b669c1fdc3606897a90e69c0210519fc2ad3
Julia Larionova <jul.larionova@gmail.com>
2018-06-18 11:58:12 +0800

    Initial commit

--- black-jack.js
+++ black-jack.js
@@ -1,1 +1,72 @@
 console.log("Привет из скрипта");
+function getSum(hand) {
+     var sum =0;
+     for (vari=0; i <hand.length; i++){
+         var card = hand [i];
+         if (card = 'A')
+         {
+             if (card == 'J' || card == 'Q' || card == 'K') {
+                 sum = sum + 10;
+
+             } else {
+                 sum = sum + parseInt(card);
+             }
+         }
+}
+for (var i=0; i<hand.length; i++){
+    var card = hand [i];
+    if (card == 'A'){
+        if (sum>10){
+            sum = +1;
+        }else{
+            sum =sum+11;
+        }
+    }
+}
+ return sum;
+}
+alert ('Дьявольское везение! BlacknJack на раздаче! $_$');
+var answer = '';
+do {
+    answer = prompt(getStatus() + 'Хотите еще карту? 1 - да, иначе - нет');
+    if (answer == '1') {
+        player.push(getCard());
+    }
+}
+var dealer = [getCard();
+var dealer = [getCard(), getCard()];
+function getStatus(){
+    return 'Дилер:' +dealer.join ('') + 'Игрок:' + player.join ('') + '.';
+}
+var dealer = [getCard()];
+var player = [getCard(), getCard()];
+if (getSum(player) ==21){
+}
+sum =getSum(player);
+if(sum>21){
+    alert ('Перебор' +getStatus());
+    break;
+}else if (sum ==21){
+    alert ('Black Jack!' + getStatus());
+    break;
+}
+}else
+{
+    while (getSum(dealear) < 17) {
+        dealer.push(getCard());
+    }
+}
+var SumDealear= getSum(dealer);
+var SumPlayer = getSum (player);
+if (SumDealer == 21) {
+    alert('У дилера перебор' + getStatus());
+} else if (sumPlayer ==sumDealer){
+    alert ('Ничья!'+ getStatus());
+} else if (SumPlayer > sumDealer){
+    alert ('Выигрыш') + getStatus();
+}else{
+    alert ('Проигрыш') + getstatus();
+}
+}
+}while (answer == '1');
+]
